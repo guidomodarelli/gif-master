6ad8643e80e1af9f949953356ad0b4395f1c66af
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-04 23:16:39 -0300

    feat: Update metadata description with detailed information

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -2,5 +2,5 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "MediaFusion Tools",
-  description: "Plataforma intuitiva con herramientas básicas para crear y editar imágenes, GIFs y videos de manera fácil y rápida. Simplifica y mejora tu contenido multimedia con MediaFusion Tools.",
+  description: "Intuitive platform with basic tools to easily and quickly create and edit images, GIFs, and videos. It includes functions for cropping, resizing, adding text and simple effects, as well as tools for converting between different video file formats and animated GIFs. Ideal for quick and simple editing tasks, without the need for heavy software.",
 };
